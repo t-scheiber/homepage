84c4bdbe0fcc4b104f4ecde1cef480f6826aae1b
Thomas Scheiber <administrator@europetalks.eu>
2026-02-15 04:07:45 +0100

    chore: get-ready (deps update, lint, build)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,7 @@
-import { defineConfig } from "eslint/config";
-import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
+import { defineConfig, globalIgnores } from "eslint/config";
+import nextVitals from "eslint-config-next/core-web-vitals";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-export default defineConfig([{
-    extends: [...nextCoreWebVitals],
-}]);
+export default defineConfig([
+  ...nextVitals,
+  globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts", "node_modules/**"]),
+]);
